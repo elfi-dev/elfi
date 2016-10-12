eb5230aafa7613ed9b345e90619455275d8bcb66
Henri Vuollekoski <henri.vuollekoski@helsinki.fi>
2016-10-12 16:29:41 +0300

    Quantile-based rejection sampling

--- abcpy/methods.py
+++ abcpy/methods.py
@@ -13,30 +13,79 @@ class ABCMethod(object):
         self.N = N
         self.distance_node = distance_node
         self.parameter_nodes = parameter_nodes
+        self.n_params = len(parameter_nodes)
         self.batch_size = batch_size
 
     def infer(self, spec, *args, **kwargs):
         raise NotImplementedError
+
+    # Run the all-accepting sampler.
+    def _get_distances(self, n_samples):
+
+        distances = self.distance_node.generate(n_samples,
+                    batch_size=self.batch_size).compute()
+        parameters = [p.generate(n_samples).compute()
+                      for p in self.parameter_nodes]
+
+        return distances, parameters
+
+    # Concatenate distances and parameters to existing.
+    def _save_distances(self, distances, parameters):
+        self.distances = np.concatenate((self.distances, distances), axis=0)
+        self.parameters = [np.concatenate((self.parameters[ii], parameters[ii]), axis=0) for ii in range(self.n_params)]
 
 
 class Rejection(ABCMethod):
     """
     Rejection sampler.
     """
-    def infer(self, threshold):
+    def infer(self, threshold=None, quantile=None):
         """
         Run the rejection sampler. Inference can be repeated with a different
         threshold without rerunning the simulator.
+        - 1st run with threshold: run simulator and apply threshold
+        - next runs with threshold: only apply threshold to existing distances
+        - all runs with quantile: run simulator
         """
 
-        # only run at first call
-        if not hasattr(self, 'distances'):
-            self.distances = self.distance_node.generate(self.N, batch_size=self.batch_size).compute()
-            self.parameters = [p.generate(self.N, starting=0).compute()
-                               for p in self.parameter_nodes]
+        # only run at first call unless quantile specified
+        if not hasattr(self, 'distances') or quantile:
+            self.distances = np.empty((0,1))
+            self.parameters = [ np.empty((0,1)) for ii in range(self.n_params) ]
 
-        accepted = self.distances < threshold
-        posteriors = [p[accepted] for p in self.parameters]
+            if quantile:
+                distances, parameters = self._get_distances(int(self.N / quantile))
+                threshold = np.percentile(distances, quantile*100)
+                discard_rest = True
+                save_values = True
+
+            else:
+                distances, parameters = self._get_distances(self.N)
+                discard_rest = False
+                save_values = True
+
+        else:  # use precomputed distances
+            distances = self.distances
+            parameters = self.parameters
+            discard_rest = False
+            save_values = False
+
+        posteriors = self.__apply_threshold(distances, parameters, threshold, discard_rest, save_values)
+
+        return {'posteriors': posteriors, 'threshold': threshold}
+
+    # Apply rejection criterion.
+    def __apply_threshold(self, distances, parameters, threshold,
+                          discard_rest=False, save_values=False):
+        accepted = distances[:,0] < threshold
+        posteriors = [p[accepted,:] for p in parameters]
+
+        if discard_rest:
+            distances = distances[accepted,:]
+            parameters = posteriors
+
+        if save_values:
+            self._save_distances(distances, parameters)
 
         return posteriors
 
