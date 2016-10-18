69e60c35256fde79212e26a8aa9ae601eff400ae
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2016-10-18 17:13:08 +0300

    Add initial draft of Bolfi

--- abcpy/methods.py
+++ abcpy/methods.py
@@ -1,3 +1,13 @@
+import numpy as np
+from time import sleep
+import dask
+from distributed import Client
+
+from .gpy_model import GpyModel
+from .acquisition import LcbAcquisition, SecondDerivativeNoiseMixin, RbfAtPendingPointsMixin
+from .utils import stochastic_optimization
+from .async import wait
+
 """
 These are sketches of how to use the ABC graphical model in the algorithms
 """
@@ -5,12 +15,12 @@ import numpy as np
 
 
 class ABCMethod(object):
-    def __init__(self, N, distance_node=None, parameter_nodes=None, batch_size=10):
+    def __init__(self, n_samples, distance_node=None, parameter_nodes=None, batch_size=10):
 
         if not distance_node or not parameter_nodes:
             raise ValueError("Need to give the distance node and list of parameter nodes")
 
-        self.N = N
+        self.n_samples = n_samples
         self.distance_node = distance_node
         self.parameter_nodes = parameter_nodes
         self.batch_size = batch_size
@@ -31,8 +41,8 @@ class Rejection(ABCMethod):
 
         # only run at first call
         if not hasattr(self, 'distances'):
-            self.distances = self.distance_node.generate(self.N, batch_size=self.batch_size).compute()
-            self.parameters = [p.generate(self.N, starting=0).compute()
+            self.distances = self.distance_node.generate(self.n_samples, batch_size=self.batch_size).compute()
+            self.parameters = [p.generate(self.n_samples, starting=0).compute()
                                for p in self.parameter_nodes]
 
         accepted = self.distances < threshold
@@ -41,43 +51,73 @@ class Rejection(ABCMethod):
         return posteriors
 
 
+class BolfiAcquisition(SecondDerivativeNoiseMixin, LcbAcquisition):
+    pass
+
+
+class AsyncBolfiAcquisition(SecondDerivativeNoiseMixin, RbfAtPendingPointsMixin, LcbAcquisition):
+    pass
+
+
 class BOLFI(ABCMethod):
 
-    def infer(self, spec, parameters=None, distance=None, threshold=None):
+    def __init__(self, n_samples, distance_node=None, parameter_nodes=None, batch_size=10, sync=True, model=None, acquisition=None, bounds=None, n_surrogate_samples=10):
+        self.n_dimensions = len(parameter_nodes)
+        self.model = model or GpyModel(self.n_dimensions, bounds)
+        self.sync = sync
+        if acquisition is not None:
+            self.acquisition = acquisition
+            self.sync = self.acquisition.sync
+        elif sync is True:
+            self.acquisition = BolfiAcquisition(self.model)
+        else:
+            self.acquisition = AsyncBolfiAcquisition(self.model, batch_size)
+        if self.sync is True:
+            self.sync_condition = "all"
+        else:
+            self.sync_condition = "any"
+        self.n_surrogate_samples = n_surrogate_samples
+        super(BOLFI, self).__init__(n_samples, distance_node, parameter_nodes, batch_size)
 
-        lik = GPLikelihoodApproximation().construct(parameters, distance)
+    def infer(self, threshold=None):
+        """
+            Bolfi inference.
 
-        # TODO
-        # - Construct PyMC model here using the lik
-        # - Run the MCMC
+            type(threshold) = float
+        """
+        self.createSurrogate()
+        return self.samplePosterior(threshold)
 
-        # Fixme: return the actual sample
-        return lik
+    def createSurrogate(self):
+        print("Sampling %d samples in batches of %d" % (self.n_surrogate_samples, self.batch_size))
+        all_values = None
+        all_locations = None
+        n_pending = 0
+        client = Client()
+        pending_indexes = list()
+        ready_indexes = list()
+        next_index = 0
+        dask.set_options(get=client.get)
+        while self.model.n_observations() < self.n_surrogate_samples:
+            pending_locations = all_values[pending_indexes] if all_values is not None and len(pending_indexes) > 0 else None
+            new_locations = self.acquisition.acquire(self.batch_size, pending_locations)
+            new_values_dict = {param.name: np.atleast_2d(new_locations[:,i]).T for i, param in enumerate(self.parameter_nodes)}
+            new_values = self.distance_node.generate(len(new_locations), with_values=new_values_dict)
+            all_locations = np.vstack((all_locations, new_locations)) if all_locations is not None else new_locations
+            all_values = all_values + new_values if all_values is not None else new_values
+            if pending_locations is not None:
+                pending_indexes = pending_indexes.extend(range(next_index, next_index + len(pending_locations)))
+                next_index += max(max(pending_locations) + 1, next_index)
+            new_ready_index = wait(list(all_values), client)  # TODO: add condition when wait() supports
+            pending_indexes.remove(new_ready_index)
+            ready_indexes.append(new_ready_index)
+            self.model.update(np.atleast_2d(all_locations[new_ready_index]), np.atleast_2d(all_values[new_ready_index]))
 
+    def getPosterior(self, threshold):
+        return None
 
-class GPLikelihoodApproximation():
-
-    def construct(self, parameters=None, distance=None):
-
-        while not self.GP.is_finished():
-            values = self.acquisition.acquire()
-            # Map the parameter values for the nodes
-            values_hash = {param.name: values[:,i] for i, param in enumerate(parameters)}
-            distances = distance.generate(len(values), self.batch_size, with_values=values_hash).compute()
-            self.GP.update(parameters, distances)
-
-        return self.GP
-
-
-
-
-
-
-
-
-
-
-
+    def samplePosterior(self, threshold):
+        return None
 
 # class SyntheticLikelihood(ABCMethod):
 #
@@ -106,11 +146,11 @@ class GPLikelihoodApproximation():
 #             parameter_values.append(values)
 #
 #         def objective(params):
-#             S = np.zeros([self.N, len(summaries)])
+#             S = np.zeros([self.n_samples, len(summaries)])
 #             y = np.zeros([1, len(summaries)])
 #             for i, s in enumerate(summaries):
-#                 parameter_values[i].values[0:self.N] = params[i]
-#                 S[:, i] = s.generate(self.N)
+#                 parameter_values[i].values[0:self.n_samples] = params[i]
+#                 S[:, i] = s.generate(self.n_samples)
 #                 y[i] = s.observed
 #             cov = np.cov(S, rowvar=False)
 #             mean = np.mean(S, axis=0)
