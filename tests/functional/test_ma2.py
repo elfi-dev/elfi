f4dc3d6862fac1d5714de3dfc2f2e7f8ef460528
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-10-18 11:08:37 +0300

    Bug fixes
    Fixed a bug from MA2 simulator which would use the same latents for all parameters in one batch run
    
    with_values now allows easier syntax (scalars and regular lists)
    
    Fixed a bug with with_values and batch_size

---
+++ tests/functional/test_ma2.py
@@ -0,0 +1,40 @@
+import time
+
+import numpy as np
+import dask
+from abcpy.core import *
+from abcpy.distributions import *
+from abcpy.examples.ma2 import MA2, autocov, distance
+from distributed import Client
+from functools import partial
+
+# Distributed setup
+# client = Client()
+# dask.set_options(get=client.get)
+
+# Setup the model
+n = 100
+t1 = 0.6
+t2 = 0.2
+
+# Set up observed data y
+latents = np.random.randn(n+2)
+y = MA2(n, 1, t1, t2, latents=latents)
+
+# Set up the simulator
+simulator = partial(MA2, n)
+
+# Set up autocovariance summaries
+ac1 = partial(autocov, 1)
+ac2 = partial(autocov, 2)
+
+# Specify the graphical model
+t1 = Prior('t1', 'uniform', 0, 1)
+t2 = Prior('t2', 'uniform', 0, 1)
+Y = Simulator('MA2', simulator, t1, t2, observed=y)
+S1 = Summary('S1', ac1, Y)
+S2 = Summary('S2', ac2, Y)
+d = Discrepancy('d', distance, S1, S2)
+
+dists = d.generate(4, with_values={'t1': .4, 't2': .1}, batch_size=1)
+print(dists.compute())
