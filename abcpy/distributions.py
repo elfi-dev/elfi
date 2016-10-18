24c580fa5a0677b4b12b1f9296075600e7c41704
Henri Vuollekoski <henri.vuollekoski@helsinki.fi>
2016-10-18 17:54:05 +0300

    Allow discrete distributions. Some renaming. Removed NumPy implementation.

--- abcpy/distributions.py
+++ abcpy/distributions.py
@@ -6,41 +6,19 @@ from functools import partial
 from . import core
 
 
-def npr_op(distribution, size, input_dict):
+def spr_op(distribution, size, input_dict):
     prng = npr.RandomState(0)
     prng.set_state(input_dict['random_state'])
-    distribution = getattr(prng, distribution)
-    size = (input_dict['n'],)+size
-    data = distribution(*input_dict['data'], size=size)
+    size = (input_dict['n'],)+tuple(size)
+    data = distribution.rvs(*input_dict['data'], size=size, random_state=prng)
     return core.to_output(input_dict, data=data, random_state=prng.get_state())
 
 
-class NumpyRV(core.RandomStateMixin, core.Operation):
+class ScipyRV(core.RandomStateMixin, core.Operation):
     """
-
-    Examples
-    --------
-    NumpyRV('tau', 'normal', 5, size=(2,3))
-
-    """
-    def __init__(self, name, distribution, *params, size=(1,)):
-        if not isinstance(size, tuple):
-            size = (size,)
-        op = partial(npr_op, distribution, size)
-        super(NumpyRV, self).__init__(name, op, *params, random_state=prng.get_state())
-
-
-def spr_op(distribution, size, args):
-    prng = npr.RandomState(0)
-    prng.set_state(args['random_state'])
-    size = (args['n'],)+tuple(size)
-    data = distribution.rvs(*args['data'], size=size, random_state=prng)
-    return core.to_output(args, data=data, random_state=prng.get_state())
-
-
-class ScipyRV_cont(core.RandomStateMixin, core.Operation):
-    """
-    Allows any distribution inheriting scipy.stats.rv_continuous
+    Allows any distribution inheriting scipy.stats.rv_continuous or
+    scipy.stats.rv_discrete. In the latter case methods pdf and logpdf
+    are mapped to pmf and logpmf.
 
     Examples
     --------
@@ -52,19 +30,25 @@ class ScipyRV_cont(core.RandomStateMixin, core.Operation):
         if not isinstance(size, tuple):
             size = (size,)
         op = partial(spr_op, distribution, size)
-        super(ScipyRV_cont, self).__init__(name, op, *params)
+        super(ScipyRV, self).__init__(name, op, *params)
 
     def pdf(self, x):
         """
         Probability density function at x of the given RV.
         """
-        return self.distribution.pdf(x, *self.params)
+        if isinstance(self, ss.rv_discrete):
+            return self.distribution.pmf(x, *self.params)
+        else:
+            return self.distribution.pdf(x, *self.params)
 
     def logpdf(self, x):
         """
         Log probability density function at x of the given RV.
         """
-        return self.distribution.logpdf(x, *self.params)
+        if isinstance(self, ss.rv_discrete):
+            return self.distribution.logpmf(x, *self.params)
+        else:
+            return self.distribution.logpdf(x, *self.params)
 
     def cdf(self, x):
         """
@@ -73,12 +57,11 @@ class ScipyRV_cont(core.RandomStateMixin, core.Operation):
         return self.distribution.cdf(x, *self.params)
 
 
-# class Prior(NumpyRV):
-class Prior(ScipyRV_cont):
+class Prior(ScipyRV):
     pass
 
 
-class Model(core.ObservedMixin, NumpyRV):
+class Model(core.ObservedMixin, ScipyRV):
     def __init__(self, *args, observed=None, size=None, **kwargs):
         if observed is None:
             raise ValueError('Observed cannot be None')
