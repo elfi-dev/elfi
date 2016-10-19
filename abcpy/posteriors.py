99f56cf58722fd945a9fa33f781ad4631467fda1
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2016-10-19 12:08:29 +0300

    Add initial draft of abstract posterior base class

--- abcpy/posteriors.py
+++ abcpy/posteriors.py
@@ -2,9 +2,74 @@ import scipy as sp
 
 from .utils import stochastic_optimization
 
-class BolfiPosterior():
+class Posterior():
+    """Container for the posterior that an .inter() method returns.
+
+    Attributes
+    ----------
+    samples : list-type
+        Pre-computed samples from the posterior.
+    """
+
+    def __init__(self):
+        self.samples = list()
+
+    def __getitem__(self, idx):
+        """ Returns samples from posterior.
+
+        Parameters
+        ----------
+        idx : slice-type
+            Indexes of samples to return
+
+        Returns
+        -------
+        list-type
+            samples
+        """
+        return self.samples[idx]
+
+    def pdf(self, x, norm=False):
+        """ Returns probability density at x.
+
+        Parameters
+        ----------
+        x : numpy 1d array
+            Location in parameter space.
+        norm : bool
+            True: density value needs to be normalized.
+            False: density value may be unnormalized.
+
+        Returns
+        -------
+        float or None
+            probability density value, or None if not implemented
+        """
+        return None
+
+    def logpdf(self, x, norm=False):
+        """ Returns log probability density at x.
+
+        Parameters
+        ----------
+        x : numpy 1d array
+            Location in parameter space.
+        norm : bool
+            True: density value needs to be normalized.
+            False: density value may be unnormalized.
+
+        Returns
+        -------
+        float or None
+            log probability density value, or None if not implemented
+        """
+        return None
+
+
+class BolfiPosterior(Posterior):
 
     def __init__(self, model, threshold, priors=None):
+        super(BolfiPosterior, self).__init__()
         self.threshold = threshold
         self.model = model
         self.priors = [None] * model.n_var
@@ -12,6 +77,19 @@ class BolfiPosterior():
         print("ML parameters: %s" % (self.ML))
         self.MAP, MAP_val = stochastic_optimization(self._neg_unnormalized_logposterior_density, self.model.bounds, 10000)
         print("MAP parameters: %s" % (self.MAP))
+
+    def logpdf(self, x, norm=False):
+        if norm is True:
+            return None
+        return self._unnormalized_loglikelihood_density(x) + self._logprior_density(x)
+
+    def pdf(self, x, norm=False):
+        if norm is True:
+            return None
+        return np.exp(self._unnormalized_logposterior_density(x))
+
+    def __getitem__(self, idx):
+        return tuple([[v]*len(idx) for v in self.MAP])
 
     def _unnormalized_loglikelihood_density(self, x):
         mean, var, std = self.model.evaluate(x)
@@ -22,12 +100,6 @@ class BolfiPosterior():
 
     def _neg_unnormalized_loglikelihood_density(self, x):
         return -1 * self._unnormalized_loglikelihood_density(x)
-
-    def _unnormalized_logposterior_density(self, x):
-        return self._unnormalized_loglikelihood_density(x) + self._logprior_density(x)
-
-    def _unnormalized_posterior_density(self, x):
-        return np.exp(self._unnormalized_logposterior_density(x))
 
     def _neg_unnormalized_logposterior_density(self, x):
         return -1 * self._unnormalized_logposterior_density(x)
@@ -45,5 +117,3 @@ class BolfiPosterior():
     def _neg_logprior_density(self, x):
         return -1 * self._logprior_density(x)
 
-    def sample(self):
-        return tuple([[v] for v in self.MAP])
