bc5e89b56529c6f3f6a328450e455e8273276092
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2016-10-20 16:16:52 +0300

    Fix few typos, add posterior call to Bolfi, refactor utils

--- abcpy/bo/utils.py
+++ abcpy/bo/utils.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-from scipy.optimize import differential_evolution
-
-def stochastic_optimization(fun, bounds, its, polish=False):
-    """ Called to find the minimum of function 'fun' in 'its' iterations """
-    result = differential_evolution(func=fun, bounds=bounds, maxiter=its,
-                                    popsize=30, tol=0.01, mutation=(0.5, 1),
-                                    recombination=0.7, disp=False,
-                                    polish=polish, init='latinhypercube')
-    return result.x, result.fun
-
 def approx_second_partial_derivative(fun, x0, dim, h, bounds):
     """
         Approximates the second derivative of function 'fun' at 'x0'
