24a2c569c13561013271167a03848ee235e8ae79
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2016-10-21 13:41:05 +0300

    Rename test_utils to bo_utils, add test for root level utils
    py.test can't handle files with same names even if in
    different locations in the directory tree (for some reason)

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,13 @@
+from abcpy.utils import stochastic_optimization
+
+class Test_stochastic_optimization():
+
+    def test_1dim_x2(self):
+        fun = lambda x : x.dot(x)
+        bounds = ((-1, 1),)
+        its = int(1e3)
+        polish=True
+        loc, val = stochastic_optimization(fun, bounds, its, polish)
+        assert abs(loc - 0.0) < 1e-5
+        assert abs(val - 0.0) < 1e-5
+
