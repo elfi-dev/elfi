fd520d5b4567032ba86ccbd7baa0790354debb76
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-10-21 12:19:16 +0300

    Allow alias strings

--- abcpy/distributions.py
+++ abcpy/distributions.py
@@ -24,9 +24,13 @@ class ScipyRV(core.RandomStateMixin, core.Operation):
     --------
     ScipyRV('tau', scipy.stats.norm, 5, size=(2,3))
     """
+
+    # Convert some common names to scipy equivalents
+    ALIASES = {'normal': 'norm'}
+
     def __init__(self, name, distribution, *params, size=(1,)):
         if isinstance(distribution, str):
-            distribution = getattr(ss, distribution)
+            distribution = getattr(ss, self.ALIASES.get(distribution, distribution))
         self.distribution = distribution
         if not isinstance(size, tuple):
             size = (size,)
