25493aef7460f87d453344df319ceb048cc5b1db
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2016-10-25 20:27:08 +0300

    Improve wrapper to handle more cases, use python string formatting
    Add relevant tests

--- tests/unit/test_wrapper.py
+++ tests/unit/test_wrapper.py
@@ -6,23 +6,48 @@ from elfi.wrapper import Wrapper
 class Test_wrapper():
 
     def test_echo_exec_arg(self):
-        command = "echo"
-        wrapper = Wrapper(command, postprocessor=int)
-        ret = wrapper.execute("1")
+        command = "echo {0}"
+        wrapper = Wrapper(command, post=int)
+        ret = wrapper("1")
         assert ret == 1
 
     def test_echo_default_arg(self):
-        command = "echo"
-        args = "1"
-        wrapper = Wrapper(command, args, postprocessor=int)
-        ret = wrapper.execute()
+        command = "echo 1"
+        wrapper = Wrapper(command, post=int)
+        ret = wrapper()
         assert ret == 1
 
     def test_echo_both_args(self):
-        command = "echo"
-        args = "1"
-        postprocessor = partial(np.fromstring, sep=" ")
-        wrapper = Wrapper(command, args, postprocessor=postprocessor)
-        ret = wrapper.execute("2")
+        command = "echo 1 {0}"
+        post = partial(np.fromstring, sep=" ")
+        wrapper = Wrapper(command, post=post)
+        ret = wrapper("2")
         assert np.array_equal(ret, np.array([1,2]))
+
+    def test_echo_kwargs(self):
+        command = "echo {param}"
+        wrapper = Wrapper(command, post=int)
+        ret = wrapper(param="1")
+        assert ret == 1
+
+    def test_echo_args_and_kwargs(self):
+        command = "echo {param} {0}"
+        post = partial(np.fromstring, sep=" ")
+        wrapper = Wrapper(command, post=post)
+        ret = wrapper(2, param="1")
+        assert np.array_equal(ret, np.array([1,2]))
+
+    def test_echo_non_string_args(self):
+        command = "echo {0}"
+        wrapper = Wrapper(command, post=int)
+        ret = wrapper(1)
+        assert ret == 1
+
+    def test_echo_parallel_args(self):
+        command = "echo {0} {1} {2}"
+        post = partial(np.fromstring, sep=" ")
+        par = (0, 1)
+        wrapper = Wrapper(command, post=post, par=par)
+        ret = wrapper(np.atleast_2d([[1],[2]]), np.atleast_2d([[3],[4]]), 5)
+        assert np.array_equal(ret, np.atleast_2d([[1, 3, 5], [2, 4, 5]]))
 
