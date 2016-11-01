b85bbe50f2a4b71a60c72c246c2649dc410f1aa6
Henri Vuollekoski <henri.vuollekoski@helsinki.fi>
2016-11-01 15:32:24 +0200

    Minor changes that address most comments in pull request #48

--- elfi/smc_abc.py
+++ elfi/smc_abc.py
@@ -4,21 +4,45 @@ from copy import copy
 
 from .methods import Rejection
 from .distributions import Prior
+from .utils import weighted_var
 
 
 class SMC(Rejection):
-    """
-    Likelihood-free sequential Monte Carlo sampler.
+    """Likelihood-free sequential Monte Carlo sampler.
 
     Based on Algorithm 4 in:
     Jean-Michel Marin, Pierre Pudlo, Christian P Robert, and Robin J Ryder:
     Approximate bayesian computational methods, Statistics and Computing,
     22(6):1167–1180, 2012.
+
+    Attributes
+    ----------
+    (as in Rejection)
+
+    See Also
+    --------
+    `Rejection` : Basic rejection sampling.
     """
 
     def infer(self, n_populations, schedule):
-        """
-        Run SMC-ABC sampler.
+        """Run SMC-ABC sampler.
+
+        Parameters
+        ----------
+        n_populations : int
+            Number of particle populations to iterate over.
+        schedule : iterable of floats
+            Acceptance quantiles for particle populations.
+
+        Returns
+        -------
+        A dictionary with items:
+        samples : list of np.arrays
+            Samples from the posterior distribution of each parameter.
+        samples_history : list of lists of np.arrays
+            Samples from previous populations.
+        weighted_sds_history : list of lists of floats
+            Weighted standard deviations from previous populations.
         """
 
         # initialize with rejection sampling
@@ -26,48 +50,58 @@ class SMC(Rejection):
         parameters = result['samples']
         weights = np.ones(self.n_samples)
 
-        # save original prior pdfs
-        orig_prior_pdfs = [copy(p.pdf) for p in self.parameter_nodes]
+        # save original priors
+        orig_priors = [p for p in self.parameter_nodes]
 
         params_history = []
+        weighted_sds_history = []
         for tt in range(1, n_populations):
-            params_history.append( list(parameters) )
+            params_history.append(parameters)
 
             weights /= np.sum(weights)  # normalize weights here
-            weighted_sds = [ np.sqrt( 2. * np.average(
-                             (p - np.average(p, weights=weights))**2,
-                                                      weights=weights) )
+
+            # calculate weighted standard deviations
+            weighted_sds = [ np.sqrt( 2. * weighted_var(p, weights) )
                              for p in parameters ]
+            weighted_sds_history.append(weighted_sds)
 
             # set new prior distributions based on previous samples
-            self.parameter_nodes = [ p.change_to( Prior(p.name, SMC_Distribution,
-                parameters[ii].copy(), weighted_sds[ii], weights ),
-                transfer_parents=False, transfer_children=True)
-                                    for ii, p in enumerate(self.parameter_nodes) ]
+            for ii, p in enumerate(self.parameter_nodes):
+                new_prior = Prior(p.name, _SMC_Distribution, parameters[ii],
+                                  weighted_sds[ii], weights)
+                self.parameter_nodes[ii] = p.change_to(new_prior,
+                                                       transfer_parents=False,
+                                                       transfer_children=True)
 
             # rejection sampling with the new priors
-            # threshold = max(np.percentile(self.distances, p_quantile*100),
-            #                 schedule[tt])
             result = super(SMC, self).infer(quantile=schedule[tt])
             parameters = result['samples']
 
             # calculate new unnormalized weights for parameters
-            # TODO: is this correct in multi-dimensional case?
-            weights_old = weights.copy()
-            weights = np.ones(self.n_samples)
+            weights_new = np.ones(self.n_samples)
             for ii in range(self.n_params):
-                weights_denom = np.sum(weights_old *
+                weights_denom = np.sum(weights *
                                        self.parameter_nodes[ii].pdf(parameters[ii]))
-                weights *= orig_prior_pdfs[ii](parameters[ii]) / weights_denom
+                weights_new *= orig_priors[ii].pdf(parameters[ii]) / weights_denom
+                # weights_new *= orig_prior_pdfs[ii](parameters[ii]) / weights_denom
+            weights = weights_new
 
-        return {'samples': parameters, 'samples_history': params_history}
+        # revert to original priors
+        self.parameter_nodes = [p.change_to(orig_priors[ii],
+                                            transfer_parents=False,
+                                            transfer_children=True)
+                                for ii, p in enumerate(self.parameter_nodes)]
+
+        return {'samples': parameters,
+                'samples_history': params_history,
+                'weighted_sds_history': weighted_sds_history}
 
 
-class SMC_Distribution(ss.rv_continuous):
+class _SMC_Distribution(ss.rv_continuous):
+    """Distribution that samples near previous values of parameters.
+    Used in SMC as priors for subsequent particle populations.
     """
-    Distribution that samples near previous values.
-    """
-    def rvs(current_params, weighted_sd, weights, size=1, random_state=None):
+    def rvs(current_params, weighted_sd, weights, random_state, size=1):
         selections = random_state.choice(np.arange(current_params.shape[0]), size=size, p=weights)
         params = current_params[selections] + \
                  ss.norm.rvs(scale=weighted_sd, size=size, random_state=random_state)
