8f85d90994001b6315f7dd18fd771d6e6489d87b
Henri Vuollekoski <henri.vuollekoski@helsinki.fi>
2016-11-09 11:49:26 +0200

    Tests for methods

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,4 +1,6 @@
-from elfi.utils import stochastic_optimization
+from elfi.utils import stochastic_optimization, weighted_var
+import numpy as np
+
 
 class Test_stochastic_optimization():
 
@@ -11,3 +13,17 @@ class Test_stochastic_optimization():
         assert abs(loc - 0.0) < 1e-5
         assert abs(val - 0.0) < 1e-5
 
+
+class Test_weighted_var():
+
+    def test_weighted_var(self):
+        data = np.random.randn(10, 4)
+        weights = np.ones((10, 1))
+        wvar = weighted_var(data, weights)
+        assert wvar.shape == (4,)
+        assert weights.shape == (10, 1)
+        assert data.shape == (10, 4)
+        assert np.allclose(wvar, np.var(data, axis=0))
+        weights[:3] = 0.
+        wvar = weighted_var(data, weights)
+        assert np.allclose(wvar, np.var(data[3:,:], axis=0))
