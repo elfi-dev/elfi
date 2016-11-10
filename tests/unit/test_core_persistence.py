056dd2955685ac784494583a28f3f54a091293f6
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-11-10 14:45:05 +0200

    Made LocalStore functional with distributed. Support for other schedulers dropped.

--- tests/unit/test_core_persistence.py
+++ tests/unit/test_core_persistence.py
@@ -16,12 +16,19 @@ def get_sleep_simulator(sleep_time=.1, *args, **kwargs):
 
 def run_cache_test(sim, sleep_time):
     t0 = timeit.default_timer()
-    sim.acquire(1).compute()
+    a = sim.acquire(1)
+    #print(a.key, a.dask)
+    a.compute()
     td = timeit.default_timer() - t0
     assert td > sleep_time
 
+    # Allow some time for the system to mark the simulation cached
+    time.sleep(0.01)
+
     t0 = timeit.default_timer()
-    sim.acquire(1).compute()
+    a = sim.acquire(1)
+    #print(a.key, a.dask)
+    a.compute()
     td = timeit.default_timer() - t0
     assert td < sleep_time
 
@@ -35,15 +42,9 @@ def test_worker_memory_cache():
 
 def test_local_object_cache():
     sleep_time = .2
-    gets = [dask.async.get_sync,
-                  dask.threaded.get,
-                  dask.multiprocessing.get,
-                  #elfi.env.client().get
-                  ]
-    for g in gets:
-        dask.set_options(get=g)
-        simfn = get_sleep_simulator(sleep_time)
-        local_store = np.zeros((10,1))
-        sim = elfi.Simulator('sim', simfn, observed=0, store=local_store)
-        run_cache_test(sim, sleep_time)
-        assert local_store[0][0] == 1
+    simfn = get_sleep_simulator(sleep_time)
+    local_store = np.zeros((10,1))
+    sim = elfi.Simulator('sim', simfn, observed=0, store=local_store)
+    run_cache_test(sim, sleep_time)
+    #sim.acquire(1).compute()
+    assert local_store[0][0] == 1
