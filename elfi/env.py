8c2d4065c1d87c0aaf39df87911ae17efe39d9cf
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-11-10 14:45:05 +0200

    Made LocalStore functional with distributed. Support for other schedulers dropped.

--- elfi/env.py
+++ elfi/env.py
@@ -1,4 +1,5 @@
 from distributed import Client, LocalCluster
+import socket
 import dask
 from collections import defaultdict
 # from dask.context import _globals as dask_globals
@@ -33,6 +34,9 @@ def get(key):
 def client():
     if _globals['client'] is None:
         # Do not start the diagnostics (bokeh) by default
-        cluster = LocalCluster(diagnostics_port=None)
+        try:
+            cluster = LocalCluster(diagnostics_port=None)
+        except (OSError, socket.error):
+            cluster = LocalCluster(scheduler_port=0, diagnostics_port=None)
         _globals['client'] = Client(cluster, set_as_default=True)
     return _globals['client']
