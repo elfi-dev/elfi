3bcebd0098c8d94b9b5bb7992058fbdc7d3012b7
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-11-21 13:58:51 +0200

    Added key version

--- tests/unit/test_core_persistence.py
+++ tests/unit/test_core_persistence.py
@@ -63,7 +63,7 @@ class TestPersistence():
         simfn = get_sleep_simulator(sleep_time)
         sim = elfi.Simulator("sim", simfn, observed=0, store=local_store)
         run_cache_test(sim, sleep_time)
-        assert local_store._read_data(sim.key_name, 0)[0] == 1
+        assert local_store._read_data(sim.id, 0)[0] == 1
 
         # Test that nodes derived from `sim` benefit from the storing
         summ = elfi.Summary("sum", lambda x : x, sim)
@@ -103,7 +103,7 @@ def test_inference_task_specific_scheduler_keys():
 
     elfi.env.client().shutdown()
 
-# TODO: 
+
 def test_reset_specific_schduler_keys():
     """This test fails if keys are not different"""
     elfi.env.client(n_workers=2, threads_per_worker=1)
@@ -113,12 +113,13 @@ def test_reset_specific_schduler_keys():
     y = None
     t = None
 
+    p1 = elfi.Prior('p', 'Uniform')
+    sim1 = elfi.Simulator('sim', lambda *args, **kwargs: args[0], p1, observed=1)
+
     for i in range(10):
         y_prev = y
         t_prev = t
 
-        p1 = elfi.Prior('p', 'Uniform')
-        sim1 = elfi.Simulator('sim', lambda *args, **kwargs: args[0], p1, observed=1)
         y = sim1.acquire(N, batch_size=bs).compute()
         t = p1.acquire(N, batch_size=bs).compute()
 
@@ -126,6 +127,6 @@ def test_reset_specific_schduler_keys():
             assert np.all(y != y_prev)
             assert np.all(t != t_prev)
 
-        elfi.new_inference_task()
+        p1.reset()
 
     elfi.env.client().shutdown()
