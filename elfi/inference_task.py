ef260310e9032233e734e915088077227b4d954f
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-11-25 16:53:46 +0200

    Fixes to PR comments

--- elfi/inference_task.py
+++ elfi/inference_task.py
@@ -1,4 +1,4 @@
-from .graph import Graph
+from elfi.graph import Graph
 
 
 class InferenceTask(Graph):
@@ -7,11 +7,14 @@ class InferenceTask(Graph):
     created in the background. One can have several named inference tasks. Each will
     have a unique name and they keep book of e.g. the seed and the nodes related to the
     inference.
+
+    All `Operation` nodes will belong to exactly one `InferenceTask`.
     """
 
+    # Index of the default named `InferenceTask` to differentiate them
     default_task_index = 0
 
-    def __init__(self, parameters=None, name=None, seed=0):
+    def __init__(self, parameters=None, name=None, seed=0, sub_stream_index=0):
         """
 
         Parameters
@@ -19,15 +22,18 @@ class InferenceTask(Graph):
         parameters : list
         name : hashable
         seed : uint32
+        sub_stream_index : uint
+
         """
         super(InferenceTask, self).__init__(name)
         self._parameters = parameters or []
         self.name = None
         self._set_name(name)
         self.seed = seed
-        self.sub_stream_index = 0
+        self.sub_stream_index = sub_stream_index
 
     def _set_name(self, name):
         if name is None:
-            self.name = "default{}".format(self.__class__.default_task_index or "")
+            name = "default{}".format(self.__class__.default_task_index or "")
             self.__class__.default_task_index += 1
+        self.name = name
