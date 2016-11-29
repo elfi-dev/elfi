69af45cf21c320bfe7c3f0991f3702b65572d294
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-11-29 12:55:49 +0200

    Add seeding to InferenceTask.
    Also added a functional test for the MA2 model.
    Various other improvements.
    Changed the signature of the autocov to match that of the notebook.

--- elfi/__init__.py
+++ elfi/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from elfi.core import *
+from elfi.core import Operation, Simulator, Summary, Discrepancy
 from elfi.distributions import *
 from elfi.methods import *
 from elfi.storage import *
 from elfi.visualization import *
+from elfi.inference_task import InferenceTask
 from elfi.env import client, inference_task, new_inference_task
 
 __author__ = 'ELFI authors'
