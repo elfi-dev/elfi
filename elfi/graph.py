4e0108fdd7cb453aaee7e16ec56c50afe38abe2c
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-12-07 12:18:16 +0200

    Temporal commit to save progress so far.

--- elfi/graph.py
+++ elfi/graph.py
@@ -101,7 +101,10 @@ class Node(object):
             self.children.insert(index, node)
 
     def __str__(self):
-        return "{}({})".format(self.__class__.__name__, self.name)
+        return "{}('{}')".format(self.__class__.__name__, self.name)
+
+    def __repr__(self):
+        return self.__str__()
 
     def is_root(self):
         return len(self.parents) == 0
