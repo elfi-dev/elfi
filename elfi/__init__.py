7dfc03b6e9695aefae6c97d13bf620cad3affae5
Henri Vuollekoski <henri.vuollekoski@aalto.fi>
2016-12-14 11:30:16 +0200

    Cleaning and updating after merge from master

--- elfi/__init__.py
+++ elfi/__init__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from elfi.core import Transform, Simulator, Summary, Discrepancy
+from elfi.core import Operation, Simulator, Summary, Discrepancy
 from elfi.distributions import *
 from elfi.methods import *
 from elfi.storage import *
 from elfi.visualization import *
 from elfi.inference_task import InferenceTask
 from elfi.env import client, inference_task, new_inference_task
-import elfi.tools
 
 __author__ = 'ELFI authors'
 __email__ = 'aki.vehtari@aalto.fi'
-__version__ = '0.2.1'
+__version__ = '0.2.x_dev'
