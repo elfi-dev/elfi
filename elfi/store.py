d002a3c9416399c10b66fc62b2748faf03f2e4dc
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2017-04-28 15:28:17 +0300

    Refactoring of ArrayPool and NpyPersistedArray

--- elfi/store.py
+++ elfi/store.py
@@ -18,6 +18,10 @@ class OutputPool:
         self.batch_size = None
         self.seed = None
 
+    def init_context(self, context):
+        self.batch_size = context.batch_size
+        self.seed = context.seed
+
     def get_batch(self, batch_index, outputs=None):
         outputs = outputs or self.outputs
         batch = dict()
@@ -54,7 +58,6 @@ class OutputPool:
             The removed store
         """
         store = self.output_stores.pop(name)
-        # TODO: should we clear the data from store
         return store
 
     def __setitem__(self, node, store):
@@ -63,12 +66,10 @@ class OutputPool:
     def __contains__(self, node):
         return node in self.output_stores
 
-    def init_context(self, context):
-        self.batch_size = context.batch_size
-        self.seed = context.seed
-
-    def destroy(self):
-        pass
+    def clear(self):
+        """Removes all data from the pool stores"""
+        for store in self.output_stores.values():
+            store.clear()
 
     @property
     def outputs(self):
@@ -77,7 +78,7 @@ class OutputPool:
 
 class ArrayPool(OutputPool):
 
-    def __init__(self, outputs, name='default', basepath=None):
+    def __init__(self, outputs, name='arraypool', basepath=None):
         super(ArrayPool, self).__init__(outputs)
 
         self.name = name
@@ -98,22 +99,29 @@ class ArrayPool(OutputPool):
     @property
     def path(self):
         if self.seed is None:
-            raise ValueError('Pool must be initialized with a context (pool.set_context)')
+            raise ValueError('Pool must be initialized with a context (pool.init_context)')
         return os.path.join(self.basepath, self.name, str(self.seed))
 
-    def destroy(self):
-        for store in self.output_stores.values():
-            store.array.close()
-
+    def delete(self):
         try:
             path = self.path
         except:
+            # Pool was not initialized
             return
-
+        self.close()
         shutil.rmtree(path)
 
+    def close(self):
+        """Closes NpyPersistedArrays"""
+        for store in self.output_stores.values():
+            if isinstance(store, BatchArrayStore) and hasattr(store.array, 'close'):
+                store.array.close()
 
-# TODO: add sqlite3 store
+    def flush(self):
+        """Flushes NpyPersistedArrays"""
+        for store in self.output_stores.values():
+            if isinstance(store, BatchArrayStore) and hasattr(store.array, 'flush'):
+                store.array.flush()
 
 
 class BatchStore:
@@ -128,10 +136,16 @@ class BatchStore:
         raise NotImplementedError
 
     def __len__(self):
+        """Number of batches in the store"""
+        raise NotImplementedError
+
+    def clear(self):
+        """Remove all batches from the store"""
         raise NotImplementedError
 
 
 class BatchArrayStore(BatchStore):
+    """Helper class to handle arrays as batch dictionaries"""
     def __init__(self, array, batch_size):
         self.array = array
         self.batch_size = batch_size
@@ -157,11 +171,15 @@ class BatchArrayStore(BatchStore):
             raise ValueError('Cannot append to array')
 
     def __len__(self):
-        return len(self.array)
+        return int(len(self.array)/self.batch_size)
 
     def _to_slice(self, batch_index):
         a = self.batch_size*batch_index
         return slice(a, a + self.batch_size)
+
+    def clear(self):
+        if hasattr(self.array, 'clear'):
+            self.array.clear()
 
 
 class NpyPersistedArray:
@@ -179,41 +197,46 @@ class NpyPersistedArray:
     HEADER_DATA_OFFSET = 12
     HEADER_DATA_SIZE_OFFSET = 8
 
-    def __init__(self, filename, array=None, mode='a'):
+    def __init__(self, name, array=None, truncate=False):
         """
 
         Parameters
         ----------
-        filename
-        mode : str
-           w : truncate and append data to the end
-           a : append data to the end
+        name : str
+            File name
+        array : ndarray, optional
+            Initial array
+        truncate : bool
+            Whether to truncate the file or not
         """
 
         self.header_length = None
+        self.itemsize = None
 
         # Header data fields
         self.shape = None
         self.fortran_order = False
         self.dtype = None
-        # The header bytes must be prepared in advance, because there is an import in
-        # numpy.lib.format._write_array_header (1.11.3) that fails if the program is being
-        # closed on exception and would corrupt the .npy file.
-        self._header_bytes = None
 
-        if filename[-4:] != '.npy':
-            filename += '.npy'
-        self.filename = filename
+        # The header bytes must be prepared in advance, because there is an import in
+        # `numpy.lib.format._write_array_header` (1.11.3) that fails if the program is
+        # being closed on exception and would corrupt the .npy file.
+        self._header_bytes_to_write = None
+
+        if name[-4:] != '.npy':
+            name += '.npy'
+        self.name = name
 
         self.fs = None
-        if mode == 'a' and os.path.exists(self.filename):
-            self.fs = open(self.filename, 'r+b')
-            self._read_header()
+        if truncate is False and os.path.exists(self.name):
+            self.fs = open(self.name, 'r+b')
+            self._init_from_file_header()
         else:
-            self.fs = open(self.filename, 'w+b')
+            self.fs = open(self.name, 'w+b')
 
         if array:
             self.append(array)
+            self.flush()
 
     def __getitem__(self, item):
         if self.header_length is None:
@@ -227,11 +250,17 @@ class NpyPersistedArray:
     def __len__(self):
         return self.shape[0] if self.shape else 0
 
+    @property
+    def size(self):
+        return np.prod(self.shape)
+
     def append(self, array):
-        """Append to the array in place"""
+        """Append data from array to self."""
+        if self.fs is None:
+            raise ValueError('Array has been deleted')
+
         if self.header_length is None:
-            self._init_file(array)
-            return
+            self._init_from_array(array)
 
         if array.shape[1:] != self.shape[1:]:
             raise ValueError("Appended array is of different shape")
@@ -243,53 +272,102 @@ class NpyPersistedArray:
         self.fs.write(array.tobytes('C'))
         self.shape = (self.shape[0] + len(array),) + self.shape[1:]
 
-        self._prepare_header_bytes()
+        # Only prepare the header bytes, need to be flushed to take effect
+        self._prepare_header_data()
 
-    def _read_header(self):
+    def _init_from_file_header(self):
+        """Initialize the object from existing file"""
         self.fs.seek(self.HEADER_DATA_SIZE_OFFSET)
-        self.shape, self.fortran_order, self.dtype = npformat.read_array_header_2_0(
+        self.shape, fortran_order, self.dtype = npformat.read_array_header_2_0(
             self.fs)
         self.header_length = self.fs.tell()
 
-    def _init_file(self, array):
-        """Sets a large header that allows the filesize to grow very large"""
-        fs = self.fs
+        if fortran_order:
+            raise ValueError('Column major (Fortran-style) files are not supported. Please'
+                             'translate if first to row major (C-style).')
 
-        # Read header data from array
+        # Determine itemsize
+        shape = (0,) + self.shape[1:]
+        self.itemsize = np.empty(shape=shape, dtype=self.dtype).itemsize
+
+    def _init_from_array(self, array):
+        """Initialize the object from an array.
+
+        Sets the the header_length so large that it is possible to append to the array.
+
+        Returns
+        -------
+        h_bytes : io.BytesIO
+            Contains the oversized header bytes
+
+        """
+        self.shape = (0,) + array.shape[1:]
+        self.dtype = array.dtype
+        self.itemsize = array.itemsize
+
+        # Read header data from array and set modify it to be large for the length
         # 1_0 is the same for 2_0
         d = npformat.header_data_from_array_1_0(array)
-        self.shape = array.shape
-        self.fortran_order = d['fortran_order']
-        self.dtype = array.dtype
-
-        # Write a fake over sized header to make it large enough for new data
-        h_bytes = io.BytesIO()
         d['shape'] = (self.MAX_SHAPE_LEN,) + d['shape'][1:]
+        d['fortran_order'] = False
+
+        # Write a prefix for a very long array to make it large enough for appending new
+        # data
+        h_bytes = io.BytesIO()
         npformat.write_array_header_2_0(h_bytes, d)
         self.header_length = h_bytes.tell()
 
-        # Write header prefix
-        fs.seek(0)
+        # Write header prefix to file
+        self.fs.seek(0)
         h_bytes.seek(0)
-        fs.write(h_bytes.read(self.HEADER_DATA_OFFSET))
+        self.fs.write(h_bytes.read(self.HEADER_DATA_OFFSET))
 
-        # Write correct header data
-        self._prepare_header_bytes()
+        # Write header data for the zero length to make it a valid file
+        self._prepare_header_data()
         self._write_header_data()
-        pos = self.fs.tell()
 
-        # Pad
-        fill_len = self.header_length - pos
-        if fill_len:
-            fs.write(b'\x20' * fill_len)
+    def truncate(self, length=0):
+        """Truncates the array to the specified length
 
-        # Write data
-        fs.write(array.tobytes('C'))
+        Parameters
+        ----------
+        length : int
+            Length (=`shape[0]`) of the array to truncate to. Default 0.
+
+        Returns
+        -------
+
+        """
+        if self.fs is None:
+            raise ValueError('Array has been deleted')
+        elif self.fs.closed:
+            raise ValueError('Array has been closed')
+
+        # Reset length
+        self.shape = (length,) + self.shape[1:]
+        self._prepare_header_data()
+        self._write_header_data()
+
+        self.fs.seek(self.header_length + self.size*self.itemsize)
+        self.fs.truncate()
 
     def close(self):
         if self.header_length:
             self._write_header_data()
             self.fs.close()
+
+    def clear(self):
+        self.truncate(0)
+
+    def delete(self):
+        """Removes the file and invalidates this array"""
+        if not self.fs:
+            return
+        name = self.fs.name
+        self.close()
+        os.remove(name)
+        self.fs = None
+        self.header_length = None
 
     def flush(self):
         self._write_header_data()
@@ -298,7 +376,7 @@ class NpyPersistedArray:
     def __del__(self):
         self.close()
 
-    def _prepare_header_bytes(self):
+    def _prepare_header_data(self):
         # Make header data
         d = {
             'shape': self.shape,
@@ -308,22 +386,26 @@ class NpyPersistedArray:
 
         h_bytes = io.BytesIO()
         npformat.write_array_header_2_0(h_bytes, d)
+
+        # Pad the end of the header
+        fill_len = self.header_length - h_bytes.tell()
+        if fill_len < 0:
+            raise OverflowError("File {} cannot be appended. The header is too short.".
+                                format(self.name))
+        elif fill_len > 0:
+            h_bytes.write(b'\x20' * fill_len)
+
         h_bytes.seek(0)
-        self._header_bytes = h_bytes.read()
+        self._header_bytes_to_write = h_bytes.read()
 
     def _write_header_data(self):
-        if not self._header_bytes:
-            # Nothing to write
+        if not self._header_bytes_to_write:
             return
-
-        if len(self._header_bytes) > self.header_length:
-            raise OverflowError("File {} cannot be appended. The header is too short.".
-                                format(self.filename))
 
         # Rewrite header data
         self.fs.seek(self.HEADER_DATA_OFFSET)
-        h_bytes = self._header_bytes[self.HEADER_DATA_OFFSET:]
+        h_bytes = self._header_bytes_to_write[self.HEADER_DATA_OFFSET:]
         self.fs.write(h_bytes)
 
         # Flag bytes off as they are now written
-        self._header_bytes = None
+        self._header_bytes_to_write = None
