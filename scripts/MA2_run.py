8f6e05f979a62d20f7b48fa87eaef46c503557da
Henri Vuollekoski <henri.vuollekoski@aalto.fi>
2017-05-18 16:19:06 +0300

    Add example script of running MA2

---
+++ scripts/MA2_run.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+import elfi
+from elfi.examples import ma2
+
+
+# load the model from elfi.examples
+model = ma2.get_model()
+
+# setup and run rejection sampling
+rej = elfi.Rejection(model['d'], batch_size=10000)
+result = rej.sample(1000, quantile=0.01)
+
+# show summary of results on stdout
+result.summary
+
+# save a figure of results
+result.plot_pairs()
+plt.savefig('ma2.png')
