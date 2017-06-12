bc5b8134aa48a5f5bb4b9d052056d6089c0fafdd
Kusti Skytén <kusti.skyten@helsinki.fi>
2017-06-12 11:31:40 +0300

    Add documentation

--- elfi/methods/post_processing.py
+++ elfi/methods/post_processing.py
@@ -2,15 +2,21 @@
 Post-processing
 """
 
-import numpy as np
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 
 class RegressionAdjustment(object):
+    """Base class for regression adjustments."""
+    _regression_model = None
+
     def __init__(self, **kwargs):
         self._model_kwargs = kwargs
         self._fitted = False
         self.regression_models =[]
+        self._X = None
+        self._result = None
+        self._parameter_names = None
 
     @property
     def parameter_names(self):
@@ -19,6 +25,7 @@ class RegressionAdjustment(object):
 
     @property
     def parameters(self):
+        """Iterator for parameter values"""
         self._check_fitted()
         
         def iterator():
@@ -29,11 +36,13 @@ class RegressionAdjustment(object):
 
     @property
     def result(self):
+        """Result object"""
         self._check_fitted()
         return self._result
 
     @property
     def X(self):
+        """The input variables"""
         self._check_fitted()
         return self._X
 
@@ -42,6 +51,19 @@ class RegressionAdjustment(object):
             raise ValueError("The regression model must be fitted first. Use the fit() method.")
 
     def fit(self, result, summary_names, observed_summaries, parameter_names):
+        """Fit a regression adjustment model to the posterior result.
+
+        Parameters
+        ----------
+        result : elfi.methods.Result
+          a result object from an ABC method
+        summary_names : list[str]
+          a list of names for the summary nodes
+        observed_summaries : array_like
+          an array of summary statistics for the observed values
+        parameter_names : list[str]
+          a list of parameter names
+        """
         self._X = _input_variables(result, observed_summaries, summary_names)
         self._result = result
         self._parameter_names = parameter_names
@@ -54,6 +76,12 @@ class RegressionAdjustment(object):
         return self._regression_model(**self._model_kwargs).fit(X, y)
 
     def adjust(self):
+        """Adjust the posterior.
+
+        Returns
+        -------
+          a numpy array with the adjusted posterior sample
+        """
         adjusted_theta = []
         for (i, theta_i) in enumerate(self.parameters):
             adjusted_theta.append(self._adjust1(theta_i, self.regression_models[i]))
@@ -65,6 +93,7 @@ class RegressionAdjustment(object):
 
 
 class LinearAdjustment(RegressionAdjustment):
+    """Regression adjustment using a local linear model."""
     _regression_model = LinearRegression
 
     def __init__(self, **kwargs):
@@ -82,6 +111,7 @@ def _input_variables(result, observed_summaries, summary_names):
 
 
 def _response(result, parameter_names):
+    """An iterator for parameter values."""
     for name in parameter_names:
         yield result.outputs[name]
 
