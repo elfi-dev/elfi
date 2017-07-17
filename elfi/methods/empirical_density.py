ad5564006e6f201dc7f4ba37a767ed921094adb6
Kusti Skytén <kusti.skyten@helsinki.fi>
2017-07-17 14:47:18 +0300

    Add ecdf

--- elfi/methods/empirical_density.py
+++ elfi/methods/empirical_density.py
@@ -1,6 +1,45 @@
 import numpy as np
 import scipy.stats as ss
 import scipy.optimize as opt
+from scipy.interpolate import interp1d
+
+
+def _ecdf(x):
+    """Compute an univariate empirical cdf."""
+    xs = np.sort(x)
+    ys = np.arange(1, len(xs) +  1)/float(len(xs))
+    return xs, ys
+
+def _low(xs, ys):
+    """Handle the lower endpoint."""
+    slope = (ys[1] - ys[0])/(xs[1] - xs[0])
+    intersect = -ys[0]/slope + xs[0]
+    return intersect
+
+def _high(xs, ys):
+    """Handle the higher endpoint."""
+    slope = (ys[-1] - ys[-2])/(xs[-1] - xs[-2])
+    intersect = (1 - ys[-1])/slope + xs[-1]
+    return intersect
+
+def ecdf(samples):
+    """Compute an interpolated cdf from empirical values."""
+    xs, ys = _ecdf(samples)
+    high = _high(xs, ys)
+    low = _low(xs, ys)
+    # add endpoints
+    x = np.append(np.insert(xs, 0, low), high)
+    y = np.append(np.insert(ys, 0, 0.), 1.)
+    f = interp1d(x, y)
+
+    def interp(x):
+        too_low = sum(x < low)
+        too_high = sum(x > high)
+        return np.concatenate([np.zeros(too_low),
+                               f(x[(x >= low) & (x <= high)]),
+                               np.ones(too_high)])
+
+    return interp
 
 
 class EmpiricalDensity(object):
@@ -21,13 +60,7 @@ class EmpiricalDensity(object):
 
     def __init__(self, samples, **kwargs):
         self.kde = ss.gaussian_kde(samples, **kwargs)
-        # Try cover the support of the pdf
-        # TODO: Use the kde
-        a, b = (min(samples), max(samples))
-        extension = (b - a)*0.5
-        self.support = (a - extension, b + extension)
-        # self.pdf.__doc__ = self.kde.pdf.__doc__
-        # self.logpdf.__doc__ = self.kde.logpdf.__doc__
+        self.ecdf = ecdf(samples)
 
     @property
     def dataset(self):
@@ -58,23 +91,6 @@ class EmpiricalDensity(object):
             return np.array([self._cdf(xi) for xi in x.flat])
         else:
             return self._cdf(x)
-
-    def _ppf(self, q):
-        return opt.brentq(lambda x: self._cdf(x) - q, *self.support)
-
-    def ppf(self, q):
-        """Percent point function (inverse of `cdf`) at q
-        of the empirical distribution.
-
-        Parameters
-        ----------
-        q : array_like
-            lower tail probability
-        """
-        if isinstance(q, np.ndarray):
-            return np.array([self._ppf(qi) for qi in q.flat])
-        else:
-            return self._ppf(q)
 
     def rvs(self, n):
         """Sample n values from the empirical density."""
