3448f012896f15c559af4520ec359b69a5c97429
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2017-07-21 10:11:27 +0300

    Change __init__ version

--- elfi/__init__.py
+++ elfi/__init__.py
@@ -17,4 +17,4 @@ __author__ = 'ELFI authors'
 __email__ = 'elfi-support@hiit.fi'
 
 # make sure __version_ is on the last non-empty line (read by setup.py)
-__version__ = '0.6.0'
+__version__ = '0.6.1'
