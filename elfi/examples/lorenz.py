Fix covariance functions implementation

elfi/examples/lorenz.py
@@ -226,22 +226,25 @@ def cov(x, side=None, lag=1):
 
     # cross-co-variance with left neighbour Y_{k-1}
     if side == 'prev':
-        return (np.mean((x[:, -lag:, :] - np.mean(x[:, -lag:, :], axis=1)) *
-                        (x[:, :-lag, :] - np.mean(x[:, :-lag, :], axis=1)),
-                        axis=1))
+        x_prev = np.roll(x[:, 1, :], 1, axis=1)
+        return np.mean((x[:, 0, :] - np.mean(x[:, 0, :], keepdims=True,
+                                             axis=1)) *
+                       (x_prev - np.mean(x_prev, keepdims=True, axis=1)), axis=1)
 
     # cross-co-variance with right neighbour Y_{k+1}
     elif side == 'next':
-        return (np.mean((x[:, :-lag, :] - np.mean(x[:, :-lag, :], axis=1)) *
-                        (x[:, -lag:, :] - np.mean(x[:, -lag:, :], axis=1)),
+        x_next = np.roll(x[:, 1, :], -1, axis=1)
+        return (np.mean((x[:, 0, :] - np.mean(x[:, 0, :], keepdims=True, axis=1)) *
+                        (x_next - np.mean(x_next, keepdims=True, axis=1)),
                         axis=1))
 
     # co-variance with neighbour Y_{k+1}
-    return (np.mean(x[:, :-1, :] * x[:, 1:, :], axis=1) -
-            np.mean(x[:, :-1, :], axis=1) * np.mean(x[:, 1:, :], axis=1))
+    x_next = np.roll(x[:, 0, :], -1, axis=1)
+    return np.mean((x[:, 0, :] - np.mean(x[:, 0, :], keepdims=True, axis=1)) *
+            (x_next - np.mean(x_next, keepdims=True, axis=1)), axis=1)
 
 
-def autocov(x, lag=1):
+def autocov(x):
     """Return the autocovariance.
 
     Assumes a (weak) univariate stationary process with mean 0.
@@ -258,7 +261,8 @@ def autocov(x, lag=1):
 
     """
 
-    C = np.mean(x[:, lag:, :] * x[:, :-lag, :], axis=1)
+    C = np.mean((x[:, 0, :] - np.mean(x[:, 0, :], keepdims=True, axis=1)) *
+            (x[:, 1, :] - np.mean(x[:, 1, :], keepdims=True, axis=1)), axis=1)
 
     return C
 