ec597343ac04cdf8d6eea5de210636e3ed8fa8ef
Xuhui Fan <xhfan.ml@gmail.com>
2018-06-29 13:14:59 +1000

    Update utils.py

--- elfi/model/utils.py
+++ elfi/model/utils.py
@@ -40,10 +40,7 @@ def distance_as_discrepancy(dist, *summaries, observed):
     # Ensure observed are 2d
     observed = np.concatenate([np.atleast_2d(o) for o in observed], axis=1)
     try:
-        if dist.keywords['metric']=='dimension_wise':
-            d = abs(summaries-observed)
-        else:
-            d = dist(summaries, observed)
+        d = dist(summaries, observed)
     except ValueError as e:
         raise ValueError('Incompatible data shape for the distance node. Please check '
                          'summary (XA) and observed (XB) output data dimensions. They '
