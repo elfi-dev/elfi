fe3af167a965562b9974bc59226d7c9d759d1926
Henri Vuollekoski <henri.vuollekoski@gmail.com>
2018-07-05 17:00:25 +0300

    Fix tests (#279)
    * Fix used client in tests
    
    * Remove mostly duplicate doctest
    
    * Tweak tests to run faster; use pytest.mark.slowtest
    
    * Fix travis for MacOS

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -30,6 +30,8 @@ def test_batch_handler(simple_model):
 
 
 def test_multiprocessing_kwargs(simple_model):
+    pre = elfi.get_client()
+
     m = simple_model
     num_proc = 2
     elfi.set_client('multiprocessing', num_processes=num_proc)
@@ -39,5 +41,7 @@ def test_multiprocessing_kwargs(simple_model):
     elfi.set_client('multiprocessing', processes=num_proc)
     rej = elfi.Rejection(m['k1'])
     assert rej.client.num_cores == num_proc
+
+    elfi.set_client(pre)
 
 # TODO: add testing that client is cleared from tasks after they are retrieved
