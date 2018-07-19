6dd3abdd8bf499302f6ab8d5f558577d8630027a
Mehti Musayev <mehty.musaev@gmail.com>
2018-07-19 16:56:28 +0300

    Fix bug for external samplers

--- elfi/visualization/progress_bar.py
+++ elfi/visualization/progress_bar.py
@@ -20,8 +20,8 @@ class ProgressBar(object):
             self.n_iter = n_sim
             self.step = ceil(n_sim / 100)
         else:
-            self.n_iter = ceil(n_samples / batch_size)
-            self.step = ceil(self.n_iter / 100)
+            self.n_iter = n_sim if n_sim else n_samples / batch_size
+            self.step = self.n_iter / 100
         self.bar = IntProgress(min=0, max=self.n_iter)
         self.label = HTML()
         self.box = VBox(children=[self.label, self.bar])
@@ -30,7 +30,7 @@ class ProgressBar(object):
     def update(self):
         self.bar.value += self.step
         self.label.value = '{name}: {index} / {size}'.format(
-            name="Inference progress bar",
+            name="Inference progress bar (number of simulations)",
             index=self.bar.value,
             size=self.n_iter,
         )
