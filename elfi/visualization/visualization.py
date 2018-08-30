98867a897cace8353db479932d60a9a019a17678
Mehti Musayev <mehty.musaev@gmail.com>
2018-08-30 12:07:56 +0300

    Fix bug with zero total number iterations in progress bar (#288)
    * Fix progress bar. Fix documentation after running linter
    
    * Remove else clause

--- elfi/visualization/visualization.py
+++ elfi/visualization/visualization.py
@@ -271,9 +271,10 @@ def progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100,
         Bar fill character
 
     """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    bar = fill * filled_length + '-' * (length - filled_length)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
-    if iteration == total:
-        print()
+    if total > 0:
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        bar = fill * filled_length + '-' * (length - filled_length)
+        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+        if iteration == total:
+            print()
