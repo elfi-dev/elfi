2ed437a091bfe82d62ced6f5905558251a817128
hpesonen <henri.e.pesonen@gmail.com>
2019-04-03 11:53:06 +0300

    Modified prob_strain

--- elfi/examples/daycare.py
+++ elfi/examples/daycare.py
@@ -75,6 +75,10 @@ def daycare(t1, t2, t3, n_dcc=29, n_ind=53, n_strains=33, freq_strains_commun=No
 
     """
     random_state = random_state or np.random
+    
+    print(t1)
+    print(t2)
+    print(t3)
 
     t1 = np.asanyarray(t1).reshape((-1, 1, 1, 1))
     t2 = np.asanyarray(t2).reshape((-1, 1, 1, 1))
@@ -91,21 +95,21 @@ def daycare(t1, t2, t3, n_dcc=29, n_ind=53, n_strains=33, freq_strains_commun=No
     # time for each DCC in the batch
     time = np.zeros((batch_size, n_dcc))
 
-    # n_factor = 1. / (n_ind - 1)
-    n_factor = 1. / n_ind
+    n_factor = 1. / (n_ind - 1)
+    # n_factor = 1. / n_ind
     gamma = 1.  # relative, see paper
 
     while np.any(time < time_end):
         with np.errstate(divide='ignore', invalid='ignore', over='ignore', under='ignore'):
         
             # probability of sampling a strain; in paper: E_s(I(t))
-            prob_strain_adjust = np.nan_to_num(state / np.sum(state, axis=3, keepdims=True)) * n_factor
+            prob_strain_adjust = np.nan_to_num(state / np.sum(state, axis=3, keepdims=True))
             prob_strain = np.sum(prob_strain_adjust,axis=2, keepdims=True)
 
         # Which individuals are already infected:
         any_infection = np.sum(state, axis=3, keepdims=True) > 0
 
-        intrainfect_rate = t1 * (np.tile(prob_strain, (1, 1, n_ind, 1)) - prob_strain_adjust) + 1e-9 
+        intrainfect_rate = t1 * (np.tile(prob_strain, (1, 1, n_ind, 1)) - prob_strain_adjust)/n_factor + 1e-9 
         alieninfect_rate = np.tile(prob_commun, (1, n_dcc, n_ind, 1)) + 1e-9
 
         # Adjust infection rates for coinfection parameters t3 
@@ -314,6 +318,6 @@ def distance(*summaries, observed):
 
     # L1 norm divided by the dimension
     dist = np.sum(np.abs(x - y), axis=(0, 2)) / (n_ss * n_dcc)
-
+    print(dist)
 
     return dist
