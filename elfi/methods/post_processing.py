ff75916a31cefb279881797380e36b4b474c37f7
hpesonen <henri.e.pesonen@gmail.com>
2020-09-02 14:06:31 +0200

    Fix docstring-example for the tests

--- elfi/methods/post_processing.py
+++ elfi/methods/post_processing.py
@@ -242,7 +242,8 @@ def adjust_posterior(sample, model, summary_names, parameter_names=None, adjustm
     >>> import elfi
     >>> from elfi.examples import gauss
     >>> m = gauss.get_model()
-    >>> res = elfi.Rejection(m['d'], output_names=['ss_mean', 'ss_var'], batch_size=10).sample(500)
+    >>> res = elfi.Rejection(m['d'], output_names=['ss_mean', 'ss_var'], \
+                             batch_size=10, bar=False).sample(500)
     >>> adj = adjust_posterior(res, m, ['ss_mean', 'ss_var'], ['mu'], LinearAdjustment())
 
     """
