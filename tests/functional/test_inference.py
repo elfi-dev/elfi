acc60eb313e32aa83130333680427a220941097b
Ulpu Remes <ulpu.remes@pm.me>
2021-01-25 17:39:33 +0100

    add functional test

--- tests/functional/test_inference.py
+++ tests/functional/test_inference.py
@@ -88,6 +88,23 @@ def test_smc():
     assert res.populations[-1].n_batches < 6
 
 
+@pytest.mark.usefixtures('with_all_clients')
+def test_adaptive_distance_smc():
+    m, true_params = setup_ma2_with_informative_data()
+
+    # use adaptive distance:
+    m['d'].become(elfi.AdaptiveDistance(m['S1'], m['S2']))
+
+    N = 1000
+    rounds = 3
+    ad_smc = elfi.ADSMC(m['d'], batch_size=20000)
+    ad_res = ad_smc.sample(N, rounds)
+
+    check_inference_with_informative_data(ad_res.samples, N, true_params)
+
+    assert len(ad_res.populations) == rounds
+
+
 @pytest.mark.slowtest
 @pytest.mark.usefixtures('with_all_clients', 'skip_travis')
 def test_BOLFI():
