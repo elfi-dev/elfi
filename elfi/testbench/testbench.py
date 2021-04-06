e9af02012c8f7ea0eddc3912b01f0e3e5ae8ca2a
hpesonen <henri.e.pesonen@gmail.com>
2021-04-06 13:28:22 +0200

    Refactor testbench.py

--- elfi/testbench/testbench.py
+++ elfi/testbench/testbench.py
@@ -34,8 +34,9 @@ class Testbench:
                  model=None,
                  repetitions=1,
                  observations=None,
-                 true_parameter=None,
-                 true_posterior=None,
+                 reference_parameter=None,
+                 reference_posterior=None,
+                 progress_bar=True,
                  seed=None):
         """Construct the testbench object.
         Parameters
@@ -46,6 +47,11 @@ class Testbench:
             How many repetitions of models is included in the testbench.
         observation : np.array, optional
             The observation if available
+        reference_parameter : np.array, optional
+            True parameter value if available
+        reference_posterior : 
+        progress_bar : boolean
+            Indicate whether to display testbench progressbar.
         seed : int, optional
 
         """
@@ -55,16 +61,55 @@ class Testbench:
         self.repetitions = repetitions
         self._set_repetition_seeds(seed)
         self.observations = observations
-        self.true_parameter = true_parameter
-        self.true_posterior = true_posterior
+        self.reference_parameter = reference_parameter
+        self.reference_posterior = reference_posterior
         self.simulator_name = list(model.observed)[0]
+        self._resolve_test_type()
+        self.progress_bar = ProgressBar(prefix='Progress', suffix='Complete',
+                                        decimals=1, length=50, fill='=')
 
     def _set_repetition_seeds(self, seed):
-        """Add a new method to the testbench."""
+        """Fix a seed for each of the repeated instances."""
         upper_limit = 2 ** 32 - 1
         self.obs_seeds = ss.randint(
             low=0, high=upper_limit).rvs(size=self.repetitions,
                                          random_state=seed)
+
+    def _resolve_test_type(self):
+        self._set_default_test_type()
+        self._resolve_reference_parameters()
+        self._resolve_observations()
+
+    def _set_default_test_type(self):
+        self.description = {
+            'observations_available': self.observations is not None,
+            'reference_parameters_available': self.reference_parameter is not None,
+            'reference_posterior_available': self.reference_posterior is not None
+            }
+
+    def _resolve_reference_parameters(self):
+        if self.description['reference_parameters_available']:
+            self.reference_parameter = np.repeat(
+                self.reference_parameter,
+                repeats=self.repetitions,
+                axis=0)
+        else:
+            self.reference_parameter = self.model.generate(
+                batch_size=self.repetitions,
+                outputs=self.model.parameter_names,
+                seed=None)
+
+    def _resolve_observations(self):
+        if self.description['observations_available']:
+            self.observations = np.repeat(
+                self.observations,
+                repeats=self.repetitions,
+                axis=0)
+        else:
+            self.observations = self.model.generate(
+                batch_size=self.repetitions,
+                with_values=self.true_parameter,
+                seed=None)
 
     def add_method(self, new_method):
         """Add a new method to the testbench.
@@ -79,26 +124,22 @@ class Testbench:
 
     def run(self):
         """Run Testbench."""
-        #repeated_result = {}
         method_result = []
         for testable_index, testable in enumerate(self.method_list):
+            self.progress_bar.reinit_progressbar(reinit_msg=testable.attributes['method'])
             # repeated_result[testable.attributes['name']] = self._repeat_test(testable)
             method_result.append(self._repeat_method_test(testable))
         print(method_result)
 
     def _repeat_method_test(self, testable):
-        
         repeated_result = []
-        self._resolve_true_parameters()
+        model_instance = self.model.copy
         for i in np.arange(self.repetitions):
-            self.model.generate(
-                batch_size=1,
-                outputs=None,
-                with_values=None,
-                seed=None)
+            self.progress_bar.update_progressbar(i + 1, self.repetitions)
+            model_instance.observed[self.simulator_name] = self.observations[i]
             # model = self.model.get_model(seed_obs=self.obs_seeds[i])
             method = testable.attributes['method'](
-                self.model, **testable.attributes['method_kwargs'])
+                model_instance, **testable.attributes['method_kwargs'])
 
             fit_kwargs = testable.attributes['fit_kwargs']
             sampler_kwargs = testable.attributes['sample_kwargs']
@@ -108,30 +149,23 @@ class Testbench:
 
             repeated_result.append(method.sample(**sampler_kwargs))
 
+        return self._combine_method_results(testable.attributes['name'],
+                                            repeated_result)
+
+    def _combine_method_results(self, name, results):
         result_dictionary = {
-            'results': repeated_result,
-            'observations': observations,
-            'true_parameters': true_parameters,
-            'true_posterior':true_posterior
+            'method': name,
+            'results': results,
+            'observations': self.observations,
+            'reference_parameters': self.reference_parameters,
+            'reference_posterior': self.reference_posterior
         }
         return result_dictionary
-
-    def _resolve_true_parameters(self):
-        if not self.true_parameter:
-            self.true_parameter = self.model.generate(
-                batch_size=self.repetitions,
-                outputs=self.model.parameter_names,
-                seed=None)
-        else: 
-            self.true_parameter = np.repeat(
-                self.true_parameter,
-                repeats=self.repetitions,
-                axis=1)
 
     def _compare_sample_results(self):
         """Compare results in sample-format."""
 
-    def _retro_fitting(self):
+    def _retrodiction(self):
         """Infer a problem with known parameter values."""
 
 
@@ -148,6 +182,7 @@ class TestSingleParameter(Testbench):
 class TestParameterDensity(Testbench):
     def __init__(self):
         super(TestParameterDensity, self).__init__()
+
 
 class TestbenchMethod:
     """Container for ParameterInference methods included in Testbench."""
@@ -175,7 +210,7 @@ class TestbenchMethod:
         seed : int, optional
 
         """
-        # name = name or method.meta['name']
+        name = name or method.__name__
         self.attributes = {'method': method,
                            'method_kwargs': method_kwargs,
                            'fit_kwargs': fit_kwargs,
