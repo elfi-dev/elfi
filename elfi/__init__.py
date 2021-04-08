0b9fb8c650069baebfbcc64f53f8dd07829f4497
hpesonen <henri.e.pesonen@gmail.com>
2021-04-08 19:40:49 +0200

    Add plot_predicted_node_pairs in __init__

--- elfi/__init__.py
+++ elfi/__init__.py
@@ -22,6 +22,7 @@ from elfi.model.extensions import ScipyLikeDistribution as Distribution
 from elfi.store import OutputPool, ArrayPool
 from elfi.visualization.visualization import nx_draw as draw
 from elfi.visualization.visualization import plot_params_vs_node
+from elfi.visualization.visualization import plot_predicted_node_pairs
 from elfi.methods.bo.gpy_regression import GPyRegression
 
 __author__ = 'ELFI authors'
