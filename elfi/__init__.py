d2d8b11b7cace065847ab653f29feda948258b90
hpesonen <henri.e.pesonen@gmail.com>
2021-04-09 10:05:56 +0200

    Rename predicted node pairplotting and add reference value plotting

--- elfi/__init__.py
+++ elfi/__init__.py
@@ -22,7 +22,7 @@ from elfi.model.extensions import ScipyLikeDistribution as Distribution
 from elfi.store import OutputPool, ArrayPool
 from elfi.visualization.visualization import nx_draw as draw
 from elfi.visualization.visualization import plot_params_vs_node
-from elfi.visualization.visualization import plot_predicted_node_pairs
+from elfi.visualization.visualization import plot_predicted_summaries
 from elfi.methods.bo.gpy_regression import GPyRegression
 
 __author__ = 'ELFI authors'
