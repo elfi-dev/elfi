275122dbc47523b9afd0d707ceb5782b07f12357
hpesonen <henri.e.pesonen@gmail.com>
2022-12-20 11:50:18 +0100

    Modify mocked modules list

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ if on_RTD:
         'scipy.sparse', 'scipy.special', 'matplotlib.pyplot', 'numpy.random', 'networkx',
         'ipyparallel', 'numpy.lib', 'numpy.lib.format', 'sklearn.linear_model',
         'sklearn.pipeline', 'sklearn.preprocessing', 'numdifftools', 'GPy.kern', 'GPy.models',
-        'sklearn.covariance', 'sklearn.exceptions'
+        'sklearn.covariance'
     ]
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
