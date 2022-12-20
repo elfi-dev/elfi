13979c48cc589a890891d5a85fc67574c04698ea
Henri Pesonen <henri.e.pesonen@gmail.com>
2022-12-20 09:44:48 +0100

    Update rtd-configuration with new mocked modules (#443)

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ if on_RTD:
         'dask.delayed', 'scipy.linalg', 'scipy.optimize', 'scipy.stats', 'scipy.spatial',
         'scipy.sparse', 'scipy.special', 'matplotlib.pyplot', 'numpy.random', 'networkx',
         'ipyparallel', 'numpy.lib', 'numpy.lib.format', 'sklearn.linear_model',
-        'sklearn.pipeline', 'sklearn.preprocessing', 'numdifftools'
+        'sklearn.pipeline', 'sklearn.preprocessing', 'numdifftools', 'GPy.kern', 'GPy.models'
     ]
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
