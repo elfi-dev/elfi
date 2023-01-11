8ca6a02870a911b1bdfc6dcd067dc865c24e669a
hpesonen <henri.e.pesonen@gmail.com>
2023-01-11 11:00:22 +0100

    Remove pygments_style in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,7 @@ add_module_names = True
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = ['elfi']
