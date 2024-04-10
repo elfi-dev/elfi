fix acquisition gradients

elfi/methods/bo/acquisition.py
@@ -295,7 +295,7 @@ class LCBSC(AcquisitionBase):
         """
         mean, var = self.model.predict(x, noiseless=True)
         grad_mean, grad_var = self.model.predictive_gradients(x)
-        value = grad_mean - 0.5 * grad_var * np.sqrt(self._beta(t) / var)
+        value = grad_mean - 0.5 * grad_var * np.nan_to_num(np.sqrt(self._beta(t) / var))
         if self.additive_cost is not None:
             value += self.additive_cost.evaluate_gradient(x)
         return value
@@ -445,7 +445,7 @@ class MaxVar(AcquisitionBase):
         a = (self.eps - mean) / scale
         b = np.sqrt(sigma2_n) / np.sqrt(sigma2_n + 2 * var)
         grad_a = (-1. / scale) * grad_mean - \
-            ((self.eps - mean) / (2. * (sigma2_n + var)**(1.5))) * grad_var
+            np.nan_to_num((self.eps - mean) / (2. * (sigma2_n + var)**(1.5))) * grad_var
         grad_b = (-np.sqrt(sigma2_n) / (sigma2_n + 2 * var)**(1.5)) * grad_var
 
         _phi_a = phi(a)