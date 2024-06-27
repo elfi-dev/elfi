aa4607b1db15fa148bd534271ca5be4182add371
Ulpu Remes <ulpu.remes@helsinki.fi>
2024-06-28 00:28:33 +0300

    make mcmc sample return idata with chains

--- elfi/methods/inference/bolfire.py
+++ elfi/methods/inference/bolfire.py
@@ -14,7 +14,7 @@ from elfi.methods.bo.utils import AdjustmentFunction, make_additive_acq
 from elfi.methods.classifier import Classifier, LogisticRegression
 from elfi.methods.inference.parameter_inference import ModelBased
 from elfi.methods.posteriors import BOLFIREPosterior
-from elfi.methods.results import BOLFIRESample
+from elfi.methods.results import MCMCSample
 from elfi.methods.utils import batch_to_arr2d, resolve_sigmas
 from elfi.model.extensions import ModelPrior
 
@@ -201,7 +201,7 @@ class BOLFIRE(ModelBased):
 
         Returns
         -------
-        BOLFIRESample
+        MCMCSample
 
         """
         # Fit posterior in case not done
@@ -282,13 +282,13 @@ class BOLFIRE(ModelBased):
 
         self.target_model.is_sampling = False
 
-        return BOLFIRESample(method_name='BOLFIRE',
-                             chains=chains,
-                             parameter_names=self.parameter_names,
-                             warmup=warmup,
-                             n_sim=self.state['n_sim'],
-                             seed=self.seed,
-                             *args, **kwargs)
+        return MCMCSample(method_name='BOLFIRE',
+                          chains=chains,
+                          parameter_names=self.parameter_names,
+                          warmup=warmup,
+                          n_sim=self.state['n_sim'],
+                          seed=self.seed,
+                          *args, **kwargs)
 
     def _resolve_marginal(self, marginal, seed_marginal=None):
         """Resolve marginal data."""
