8b27bc47b9e534885b1613d6330da79ad1ab9323
Yannick Kuhn <yannick.kuhn@dlr.de>
2025-02-27 11:02:48 +0100

    removed duplicate PyTorch example acquisition function

--- elfi/methods/bo/botorch_acquisition.py
+++ elfi/methods/bo/botorch_acquisition.py
@@ -132,17 +132,3 @@ class BoTorchLCBSC(BoTorchAcquisitionBase):
         )
         candidate = get_best_candidates(batch_candidates, -batch_acq_values)
         return candidate.detach().numpy()
-
-
-class SOBERUCB:
-    def __init__(self, model, label="UCB", sample_size=1, exploration_rate=10):
-        self.label = label
-        self.exploration_rate = exploration_rate
-        self.beta = (2 * log(
-            sample_size**(2 * model.dim + 2) * pi**2
-            / (3 / self.exploration_rate)
-        ))
-        self.af = UpperConfidenceBound(model, beta=self.beta)
-
-    def __call__(self, x):
-        return self.af(x.unsqueeze(1)).detach()
